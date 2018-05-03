305876b60537635a4a5b01bce08c38def3b098f4
Martín Villanueva <martin.villanueva@usm.cl>
2018-05-03 16:11:31 -0300

    fixes of setup.py with platforms: Linux and osx

--- lib/setup.py
+++ lib/setup.py
@@ -2,13 +2,22 @@ from distutils.core import setup
 from Cython.Build import cythonize
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import platform
+
+
+
+if "Linux" in platform.platform():
+    extra_link_args = ['-fopenmp']
+else:
+    extr_link_args = ['-fopenmp', "-Wl,-rpath,/usr/local/opt/gcc/lib/gcc/7/"]
+
 
 ext_modules=[
     Extension("fgm_eval",
               ["fgm_eval.pyx"],
               libraries=["m"],
               extra_compile_args = ["-O3", "-ffast-math", "-march=native", "-fopenmp"],
-              extra_link_args=['-fopenmp', "-Wl,-rpath,/usr/local/opt/gcc/lib/gcc/7/"]
+              extra_link_args=extra_link_args
               ) 
 ]
 
