d8c231e27415b009dee2d68eb083563c090a3f18
Martín Villanueva <martin.villanueva@usm.cl>
2018-05-07 18:20:45 -0300

    Updated make_fgm.sh to work in linux hosts

--- lib/make_fgm.sh
+++ lib/make_fgm.sh
@@ -2,5 +2,5 @@
 rm -rf build
 rm fgm_eval.c
 python setup.py build
-cp build/lib.macosx-10.7-x86_64-3.6/fgm_eval.cpython-36m-darwin.so ./fgm_eval.so
+cp build/lib.*/fgm_eval.*.so ./fgm_eval.so
 echo "DONE!"
