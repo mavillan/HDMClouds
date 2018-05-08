22d6990e47f01a68f401025a7f2d26563b010667
Martín Villanueva <martin.villanueva@usm.cl>
2018-05-08 14:00:56 -0300

    fixed big with openmp

--- lib/setup.py
+++ lib/setup.py
@@ -4,20 +4,12 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import platform
 
-
-
-if "Linux" in platform.platform():
-    extra_link_args = ['-fopenmp']
-else:
-    extra_link_args = ['-fopenmp', "-Wl,-rpath,/usr/local/opt/gcc/lib/gcc/8/"]
-
-
 ext_modules=[
     Extension("fgm_eval",
               ["fgm_eval.pyx"],
               libraries=["m"],
               extra_compile_args = ["-O3", "-ffast-math", "-march=native", "-fopenmp"],
-              extra_link_args=extra_link_args
+              extra_link_args = ['-fopenmp']
               ) 
 ]
 
