11a55fef072e8907a3698a1af7a4e0bdf8f0ebad
Martín Villanueva <martin.villanueva@usm.cl>
2018-06-14 11:20:39 -0500

    Added support for 3D data in compute_mask function

--- lib/preprocessing.py
+++ lib/preprocessing.py
@@ -18,14 +18,28 @@ umap = {'RA':'RA (J2000)', 'DEC':'Dec (J2000)',
 def compute_mask(data, back_level, min_obj_size=20, min_hole_size=10):
     # thresholding
     mask = data>=back_level
-    # structuring elements
-    disk1 = disk(1)
-    square2 = square(2)
-    # cleaning
-    mask = binary_opening(mask, selem=disk1)
-    mask = remove_small_objects(mask.astype(bool), min_size=min_obj_size)
-    mask = binary_closing(mask, selem=square2)
-    mask = remove_small_holes(mask, min_size=min_hole_size)
+    if data.ndim==2:
+        # structuring elements
+        disk1 = disk(1)
+        square2 = square(2)
+        # cleaning
+        # opening: remove small bright spots and connect small dark cracks
+        mask = binary_opening(mask, selem=disk1)
+        mask = remove_small_objects(mask.astype(bool), min_size=min_obj_size)
+        # closing: remove small dark spots and connect small bright cracks
+        mask = binary_closing(mask, selem=square2)
+        mask = remove_small_holes(mask, min_size=min_hole_size)
+    if data.ndim==3:
+        # structuring elements
+        ball1 = ball(1)
+        cube2 = cube(2)
+        # cleaning
+        # opening: remove small bright spots and connect small dark cracks
+        mask = binary_opening(mask, selem=ball1)
+        mask = remove_small_objects(mask, min_size=min_obj_size)
+        # closing: remove small dark spots and connect small bright cracks
+        mask = binary_closing(mask, selem=cube2)
+        mask = remove_small_holes(mask, min_size=min_hole_size)
     return mask
 
 
