d667765ef3387efaa991d797d5cbf519443b5513
Martín Villanueva <martin.villanueva@usm.cl>
2018-10-21 18:58:17 -0300

    Minor fix to setup2.py

--- lib/setup2.py
+++ lib/setup2.py
@@ -3,18 +3,21 @@ from Cython.Build import cythonize
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import platform
+import numpy as np
 
 ext_modules=[
     Extension("gmr_cython",
               ["gmr_cython.pyx"],
               libraries=["m"],
               extra_compile_args = ["-O3", "-ffast-math", "-march=native", "-fopenmp"],
-              extra_link_args = ['-fopenmp']
+              extra_link_args = ['-fopenmp'],
+              include_dirs = [np.get_include()]
               ) 
 ]
 
 setup( 
   name = "gmr_cython",
   cmdclass = {"build_ext": build_ext},
-  ext_modules = ext_modules
+  ext_modules = ext_modules,
+  include_dirs = [np.get_include()]
 )
