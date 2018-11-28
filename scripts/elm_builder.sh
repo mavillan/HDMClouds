7183a166d7cf38097bf0e60c5ac46db0866342d1
mavillan <nallivam@gmail.com>
2018-11-28 23:43:47 +0000

    removed old elm files

--- scripts/elm_builder.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#PBS -l cput=150:00:00
-#PBS -l walltime=150:00:00
-
-#use anaconda2
-#python /user/m/marvill/VarClump/scripts/elm_builder.py $1 $2 $3
-/user/m/marvill/anaconda2/bin/python2 /user/m/marvill/VarClump/scripts/elm_builder.py $1 $2 $3
